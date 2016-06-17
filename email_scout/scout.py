1b902b07674fbd772c304d22518897b09f6823fc
Slava Tkach <Slava Tkach>
2016-06-17 19:13:16 +0300

    add -- fix bugs,make as class, write output in .txt file

--- email_scout/scout.py
+++ email_scout/scout.py
@@ -2,91 +2,108 @@
 # -*- coding: utf-8 -*-
 # Python 3.4
 # made by  Xtr3am3r.0k@gmail.com
+# version 0.0.1
 # pip install bs4
-import re
+import re, os
 import argparse
-import urllib.request
-import urllib.parse
+import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 
+
+class Spider:
+    def __init__(self):
+        self.tasklist = []
+        self.perfected = []
+        self.fond_mails = {}
+
+    def add_task(self, request):
+        main_link = urllib.parse.urlparse(request)
+        if request in self.perfected:
+            pass
+        elif request in self.tasklist:
+            pass
+        else:
+            self.tasklist.append(request)
+
+    def srch_page(self,request):
+        html = urllib.request.urlopen(request).read().decode('utf-8', errors='ignore')
+        mailsrch = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}')
+        found = mailsrch.findall(html)
+        for item in found:
+            self.fond_mails[item] = 1
+    def spider_searcher(self, request):
+        found_links = []
+        search = re.compile(r'^/\w+')
+        html = urllib.request.urlopen(request).read().decode('utf-8', errors='ignore')
+        pars_link = urllib.parse.urlparse(request)
+        soup = (BeautifulSoup(html, "html.parser")).findAll('a')
+        for link in soup:
+            newlink = str(link).split(' ')[1].split('"')[1]
+            if 'http' in newlink:
+                found_links.append(newlink)
+            elif search.findall(newlink):
+                found_links.append(newlink)
+            else:
+                pass
+        for url in found_links:
+            request_link = urllib.parse.urlparse(url)
+            self.add_task(pars_link[0] + '://' + pars_link[1] + request_link[2])
+
+
+    def spider(self):
+        while len(self.tasklist) != 0:
+            for item in self.tasklist:
+                self.printall(item)
+                try:
+                    self.spider_searcher(item)
+                except (KeyboardInterrupt, EOFError) as err:
+                    raise
+                except:
+                    pass
+                try:
+                    self.srch_page(item)
+                except (KeyboardInterrupt, EOFError) as err:
+                    raise
+                except:
+                    pass
+                self.perfected.append(str(item))
+                del self.tasklist[self.tasklist.index(item)]
+    def __str__(self):
+        found= ''
+        for items in self.fond_mails:
+            found += '{}\n'.format(items)
+        with open('found_emails.txt','w') as file:
+            file.write(found)
+            file.close()
+        return found
+
+    def printall(self, request):
+        print('+' * 64, '\n [+] current link in process = {}'.format(request),
+                '\n [*] request links left= {}\n'.format(len(self.tasklist)),
+                '[*] response links status= {}\n'.format(len(self.perfected)),
+                '[*] Emails found = {}\n'.format(len(self.fond_mails)),
+                'Press Ctrl+C to exit\n')
+
+def main():
+    site_parse = Spider()
+    site_parse.add_task(args['url'])
+    print('Start Program...')
+    try:
+        site_parse.spider()
+    except KeyboardInterrupt:
+        print('\n\n[-] KeyboardInterrupt = Exit')
+    print('Program Ends...')
+    print(site_parse,'\n [+] all results write in found_emails.txt')
+
+
+
 parser = argparse.ArgumentParser(description='Site Email spider')
-parser.add_argument('-u','--url', help='set url in format http://exemple.com', required=True)
+parser.add_argument('-u', '--url', help='set url in format http://exemple.com', required=True)
 args = vars(parser.parse_args())
 
-tasklist = []
-perfected = []
-fond_mails = {}
-
-def site_url(request):
-    main_link = urllib.parse.urlparse(request)
-    if request in perfected:
-        pass
-    elif request in tasklist:
-        pass
-    else:
-        tasklist.append(request)
-
-def page_srch(request):
-    try:
-        html = urllib.request.urlopen(request).read().decode('utf-8', errors='ignore')
-    except:
-        html = urllib.request.urlopen(perfected[0]).read().decode('utf-8', errors='ignore')
-    mailsrch = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}')
-    found = mailsrch.findall(html)
-    for item in found:
-        fond_mails[item] = 1
-
-def spider_searcher(request):
-    found_links = []
-    search = re.compile(r'^/\w+')
-    try:
-        html = urllib.request.urlopen(request).read().decode('utf-8', errors='ignore')
-    except:
-        html = urllib.request.urlopen(perfected[0]).read().decode('utf-8', errors='ignore')
-    pars_link = urllib.parse.urlparse(request)
-    soup = (BeautifulSoup(html, "html.parser")).findAll('a')
-    for link in soup:
-        newlink = str(link).split(' ')[1].split('"')[1]
-        if 'http' in newlink:
-            found_links.append(newlink)
-        elif search.findall(newlink):
-            found_links.append(newlink)
-        else:
-            pass
-    for url in found_links:
-        request_link = urllib.parse.urlparse(url)
-        site_url(pars_link[0] + '://' + pars_link[1] + request_link[2])
 
 
-def spider():
-    try:
-        while len(tasklist) != 0:
-            for item in tasklist:
-                print('++++'*64)
-                print('[*] request links left=', len(tasklist))
-                print('[*] response links status=', len(perfected))
-                print('[*] Emails found =', len(fond_mails))
-                print('Press Ctrl+C to exit')
-                spider_searcher(item)
-                page_srch(item)
-                perfected.append(str(item))
-                del tasklist[tasklist.index(item)]
-            print('----' * 64)
-            print('[+] STATUS..............')
-            print('[*] request links left=', len(tasklist))
-            print('[*] response links status=', len(perfected))
-            print('[*] Emails found =', len(fond_mails))
-            for items in fond_mails:
-                print(items)
-
-    except KeyboardInterrupt:
-        print('----' * 64)
-        print('[+] STATUS..............')
-        print('[*] request links left=', len(tasklist))
-        print('[*] response links status=', len(perfected))
-        print('[*] Emails found =', len(fond_mails))
-        for items in fond_mails:
-            print(items)
 if __name__ == "__main__":
-    site_url(args['url'])
-    spider()
+    main()
+
+

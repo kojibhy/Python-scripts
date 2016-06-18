1a1febf2e20afd7694dcc83c50e4c9495806b251
Slava Tkach <Slava Tkach>
2016-06-18 11:44:59 +0300

    fix

--- email_scout/scout.py
+++ email_scout/scout.py
@@ -27,7 +27,7 @@ class Spider:
 
     def srch_page(self, request):
         html = urllib.request.urlopen(request).read().decode()
-        mailsrch = re.compile(r"([\w\.\-_]+)?\w+@[\w\.\-_]+(\.\w+){1,}")
+        mailsrch = re.compile(r"/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+/g")
         found = mailsrch.findall(html)
         for item in found:
             self.fond_mails[item] = 1

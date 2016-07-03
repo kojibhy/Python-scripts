48a21c9be5fda25a2820834b0b10831716e4bce4
Slava Tkach <Slava Tkach>
2016-07-03 08:25:35 +0300

    -- add fix + ru language support

--- email_spoofing/simple_spoofer.py
+++ email_spoofing/simple_spoofer.py
@@ -3,32 +3,45 @@
 # Python 3.4
 # made by  Xtr3am3r.0k@gmail.com
 
-
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.message import Message
+from email.header import make_header as mkh
+from email.utils import make_msgid as msgid
+from email.utils import formatdate
+
 
 def sender():
-    smtp_host_user = ""
-    smtp_host_password = ""
-    smtp_host = ''
-    smtp_port = 465
-    
-    fromaddress = smtp_host_user
-    toaddress = ''
+    # smtp config loading
+    login = ''
+    password = ''
+    host = ''
+    port = '' #port mast be int
 
+    #request config loading
+    request_from = '' #From email
+    request_to = '' #To email
+    request_view = ''#
+    request_subject = '' #
+    request_text ='' #
     
+    msg = Message()
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = ""
-    msg['From'] =  ""
-    msg['To'] = toaddress
-    msg.attach(MIMEText('<h1> Sender hello word from all citizens</h1>', 'html'))
-    server = smtplib.SMTP_SSL(smtp_host, smtp_port)
-    server.ehlo()
-    print(server.ehlo())
-    server.login(smtp_host_user,smtp_host_password)
-    print(server.login(smtp_host_user, smtp_host_password))
-    server.sendmail(fromaddress, toaddress,msg.as_string())
-    server.quit()
+    msg['Message-ID'] = msgid()
+    msg['Date'] = formatdate(localtime=True)
+    msg['From'] = mkh([('{}'.format(request_view), 'UTF-8'), ('<' + request_from + '>', 'us-ascii')])
+    msg['To'] = request_to
+    msg['Subject'] = request_subject
+    msg['User-Agent'] = 'Horde Application Framework 5' # UA Exemple
+    msg['Content-Disposition'] = "inline"
+    msg['Content-Transfer-Encoding'] = "8bit"
+    msg.attach(MIMEText(request_text, _subtype='html', _charset='utf-8'))
+    msg.preamble = "This is a multi-part message in MIME format."
+    msg.epilogue = "End of message"
 
-sender()
+    server = smtplib.SMTP_SSL(host, port)
+    server.login(login, password)
+    server.ehlo()
+    server.sendmail(request_from, request_to, msg.as_string())
+    server.quit()

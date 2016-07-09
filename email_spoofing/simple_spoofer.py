d19632ac748e83ed216314a2b7fd0a6068067964
Slava Tkach <Slava Tkach>
2016-07-09 13:37:41 +0300

    -add class view

--- email_spoofing/simple_spoofer.py
+++ email_spoofing/simple_spoofer.py
@@ -6,42 +6,54 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.message import Message
-from email.header import make_header as mkh
-from email.utils import make_msgid as msgid
+from email.header import make_header
+from email.utils import make_msgid
 from email.utils import formatdate
 
 
-def sender():
-    # smtp config loading
-    login = ''
-    password = ''
-    host = ''
-    port = '' #port mast be int
+class EmailSender():
+    def __init__(self, **kwargs):
+        self.login = kwargs['login']
+        self.password = kwargs['password']
+        self.host = kwargs['host']
+        self.port = int(kwargs['port'])
+        self.from_email = kwargs['from_email']
+        self.from_text = kwargs['from_text']
+        self.to = kwargs['to']
+        self.subject = kwargs['subject']
+        self.message_text = kwargs['message_text']
 
-    #request config loading
-    request_from = '' #From email
-    request_to = '' #To email
-    request_view = ''#
-    request_subject = '' #
-    request_text ='' #
-    
-    msg = Message()
-    msg = MIMEMultipart('alternative')
-    msg['Message-ID'] = msgid()
-    msg['Date'] = formatdate(localtime=True)
-    msg['From'] = mkh([('{}'.format(request_view), 'UTF-8'), ('<' + request_from + '>', 'us-ascii')])
-    msg['To'] = request_to
-    msg['Subject'] = request_subject
-    msg['User-Agent'] = 'Horde Application Framework 5' # UA Exemple
-    msg['Content-Disposition'] = "inline"
-    msg['Content-Transfer-Encoding'] = "8bit"
-    msg.attach(MIMEText(request_text, _subtype='html', _charset='utf-8'))
-    msg.preamble = "This is a multi-part message in MIME format."
-    msg.epilogue = "End of message"
+    def masseges(self):
+        msg = MIMEMultipart()
+        msg['Message-ID'] = make_msgid()
+        msg['Date'] = formatdate(localtime=True)
+        msg['From'] = make_header([('{}'.format(self.from_text), 'UTF-8'), ('<' + self.from_email + '>', 'us-ascii')])
+        msg['To'] = self.to
+        msg['Subject'] = self.subject
+        msg['User-Agent'] = 'Horde Application Framework 5'
+        msg['Content-Disposition'] = "inline"
+        msg['Content-Transfer-Encoding'] = "8bit"
+        msg.attach(MIMEText(self.message_text, _subtype='html', _charset='utf-8'))
+        return(msg)
 
-    server = smtplib.SMTP_SSL(host, port)
-    server.login(login, password)
-    server.ehlo()
-    server.sendmail(request_from, request_to, msg.as_string())
-    server.quit()
+    def chainsend(self):
+        server = smtplib.SMTP_SSL(self.host, self.port)
+        authentication = server.login(self.login, self.password)
+        echo = server.ehlo()
+        awere = server.sendmail(self.from_email, self.to, self.masseges().as_string())
+        server.quit()
+
+
+if __name__ == '__main__':
+    print('Debug mode')
+    d = EmailSender(**{'login': '',
+                       'password': '',
+                       'host': 'smtp.mail.ru',
+                       'port': 465,
+                       'from_email': '',
+                       'from_text': '',
+                       'to': '',
+                       'subject': '',
+                       'message_text': '<img src="" width="500" height="900" align="center" />'
+                       })
+    d.chainsend()
